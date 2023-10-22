9fd41ec2145587fc249f53e463b9edf88ed6fb24
Giovanni Mugelli <mugelli.giovanni@gmail.com>
2023-10-22 11:38:47 +0200

    first commit

---
+++ wamih/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  wamih
+//
+//  Created by Giovanni Mugelli on 18/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wamih/wamihApp.swift
@@ -0,0 +1,17 @@
+//
+//  wamihApp.swift
+//  wamih
+//
+//  Created by Giovanni Mugelli on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct wamihApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
